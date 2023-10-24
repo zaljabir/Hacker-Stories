07339f2c73fd4a6b16eb803a664e4639e14635e6
zaljabir <56142717+zaljabir@users.noreply.github.com>
2023-10-24 12:24:57 -0700

    react jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,22 @@
 import * as React from "react";
 
-const title = 'React';
+function getTitle(title) {
+  return title;
+}
+
+const age = 26;
+
+const produce = ["Apples", "Bananas", "Oranges"];
 
 function App() {
   return (
     <div>
-      <h1>Hello {title}</h1>
+      <h1>Hello {getTitle('React')} I am {age} years old.</h1>
+
+      <label htmlFor="search">Search: </label>
+      <input id="search" type="text" />
+      <p htmlFor="fruit">Fruit: </p>
+      {produce.map(produce => <li key={produce}>{produce}</li>)}
     </div>
   );
 }
