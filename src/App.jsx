c8e5756c1d77303ae5aeb22e985a7caa6f7988ba
zaljabir <56142717+zaljabir@users.noreply.github.com>
2023-10-26 12:29:30 -0700

    handler functions in jsx

--- src/App.jsx
+++ src/App.jsx
@@ -62,11 +62,22 @@ const List = () => (
       </ul>
 );
 
-const Search = () => (
+const Search = () => {
+  const handleChange = (event) => {
+    console.log(event);
+    console.log(event.target.value);
+  }
+
+  const handleBlur = (event) => {
+    console.log(event);
+    console.log(event.target.value);
+  }
+  return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" />
+      <input id="search" type="text" onChange={handleChange} onBlur={handleBlur} />
     </div>
-);
+  );
+} 
 
 export default App;
