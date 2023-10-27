3a7900503cd11afcca00790fce5f4096a566d3af
zaljabir <56142717+zaljabir@users.noreply.github.com>
2023-10-27 14:18:11 -0700

    react props

--- src/App.jsx
+++ src/App.jsx
@@ -1,41 +1,43 @@
 import * as React from "react";
 
-const list = [
-  {
-    title: 'React',
-    url: 'https://reactjs.org/',
-    author: 'Jordan Walke',
-    num_comments: 3,
-    points: 4,
-    objectID: 0,
-  },
-  {
-    title: 'Redux',
-    url: 'https://redux.js.org/',
-    author: 'Dan Abramov, Andrew Clark',
-    num_comments: 2,
-    points: 5,
-    objectID: 1,
-  },
-  {
-    title: 'Firebase',
-    url: 'https://firebase.google.com/',
-    author: 'James Tamplin, Andrew Lee',
-    num_comments: 4,
-    points: 2,
-    objectID: 2,
-  },
-  {
-    title: 'Vue',
-    url: 'https://vuejs.org/',
-    author: 'Evan You',
-    num_comments: 0,
-    points: 1,
-    objectID: 3,
-  },
-];
+const App = () => {
 
-const App = () => (
+  const stories = [
+    {
+      title: 'React',
+      url: 'https://reactjs.org/',
+      author: 'Jordan Walke',
+      num_comments: 3,
+      points: 4,
+      objectID: 0,
+    },
+    {
+      title: 'Redux',
+      url: 'https://redux.js.org/',
+      author: 'Dan Abramov, Andrew Clark',
+      num_comments: 2,
+      points: 5,
+      objectID: 1,
+    },
+    {
+      title: 'Firebase',
+      url: 'https://firebase.google.com/',
+      author: 'James Tamplin, Andrew Lee',
+      num_comments: 4,
+      points: 2,
+      objectID: 2,
+    },
+    {
+      title: 'Vue',
+      url: 'https://vuejs.org/',
+      author: 'Evan You',
+      num_comments: 0,
+      points: 1,
+      objectID: 3,
+    },
+  ];
+  
+  return (
     <div>
       <h1>My Hacker Stories</h1>
 
@@ -43,23 +45,28 @@ const App = () => (
 
       <hr />
 
-      <List />
+      <List list={stories}/>
     </div>
-);
+  );
+}
 
-const List = () => (
+const List = (props) => (
       <ul>
-        {list.map((item) => (
-            <li key={item.objectID}>
-              <span>
-                <a href={item.url}> {item.title} </a>
-              </span>  
-              <span> {item.author} </span>
-              <span> {item.num_comments} </span>
-              <span> {item.points} </span>
-            </li>
+        {props.list.map((item) => (
+            <Item key={item.objectID} item={item}/>
           ))}
       </ul>
+);
+
+const Item = (props) => (
+  <li>
+    <span>
+      <a href={props.item.url}> {props.item.title} </a>
+    </span>  
+    <span> {props.item.author} </span>
+    <span> {props.item.num_comments} </span>
+    <span> {props.item.points} </span>
+  </li>
 );
 
 const Search = () => {
