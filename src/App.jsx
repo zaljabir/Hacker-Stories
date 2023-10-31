4432ec56713509a9eec3724a11c0bfa3c9eaa7a2
zaljabir <56142717+zaljabir@users.noreply.github.com>
2023-10-31 16:10:01 -0700

    react controlled components

--- src/App.jsx
+++ src/App.jsx
@@ -36,7 +36,7 @@ const App = () => {
       objectID: 3,
     },
   ];
-  const [searchTerm, setSearchTerm] = React.useState('');
+  const [searchTerm, setSearchTerm] = React.useState('React');
 
   const handleSearch = (event) => {
     setSearchTerm(event.target.value);
@@ -48,7 +48,7 @@ const App = () => {
     <div>
       <h1>My Hacker Stories</h1>
 
-      <Search onSearch={handleSearch} />
+      <Search search={searchTerm} onSearch={handleSearch} />
 
       <hr />
 
@@ -91,7 +91,7 @@ const Search = (props) => {
   return (
     <div>
       <label htmlFor="search">Search: </label>
-      <input id="search" type="text" onChange={props.onSearch} onBlur={handleBlur} />
+      <input id="search" type="text" value={props.search} onChange={props.onSearch} onBlur={handleBlur} />
 
       <p>
         
